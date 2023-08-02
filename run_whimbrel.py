6f01f4ab77156789e97ec00b554ec0bc198057e6
Eric Matthes <ehmatthes@gmail.com>
2023-08-02 07:38:24 -0800

    pexpect can still run whimbrel.

--- run_whimbrel.py
+++ run_whimbrel.py
@@ -12,16 +12,19 @@ child = pexpect.spawn('python whimbrel.py')
 child.expect("WHIMBREL", timeout=3)
 # stream.feed(child.before.decode() + child.after.decode())
 
+child.send('T')
+child.expect("WHIMBREL", timeout=3)
+
 child.send('H')
-child.expect("WHIMBREL\r\n\r\nH", timeout=3)
+child.expect("Quit\r\n\r\nH", timeout=3)
 # stream.feed(child.before.decode() + child.after.decode())
 
 child.send('e')
-child.expect("WHIMBREL\r\n\r\nHe", timeout=3)
+child.expect("Quit\r\n\r\nHe", timeout=3)
 # stream.feed(child.before.decode() + child.after.decode())
 
 child.send('l')
-child.expect("WHIMBREL\r\n\r\nHel", timeout=3)
+child.expect("Quit\r\n\r\nHel", timeout=3)
 # stream.feed(child.before.decode() + child.after.decode())
 
 # for c in 'ello':
