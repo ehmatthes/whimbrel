c748f5bfa8e6e5c8e36b7b3c3bb2804e941c844b
Eric Matthes <ehmatthes@gmail.com>
2023-08-02 09:16:14 -0800

    Verifies that tests are examining actual output.

--- tests/integration_tests/test_core_functionality.py
+++ tests/integration_tests/test_core_functionality.py
@@ -1,2 +1,14 @@
-def test_read_file():
-    pass
+import pexpect
+import pytest
+
+def test_run_whimbrel():
+    """Test that I can run Whimbrel, and see expected output."""
+    child = pexpect.spawn('python whimbrel.py')
+    child.expect("WHIMBREL", timeout=0.1)
+
+@pytest.mark.xfail
+def test_run_whimbrel_expect_fail():
+    """Test that I can run Whimbrel, and fail to see unexpected output."""
+    child = pexpect.spawn('python whimbrel.py')
+    child.expect("WHIMBRELLLL", timeout=0.1)
+
