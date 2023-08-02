b4b0896ee0a35ce3a44a379859d7f55e5105d75e
Eric Matthes <ehmatthes@gmail.com>
2023-08-01 23:15:20 -0800

    Buffer works, includes diagnostics.

--- whimbrel.py
+++ whimbrel.py
@@ -29,14 +29,37 @@ class Whimbrel:
         print("WHIMBREL\n")
         
         # Display the current buffer.
-        if self.buffer:
-            print(self.buffer, end="", flush=True)
+        if not self.buffer:
+            return
+
+        # We have some data, so show it.
+        # if self.buffer[-1] == "\r":
+        #     print(self.buffer)
+        # else:
+        #     print(self.buffer, end="", flush=True)
+
+        # if self.buffer[-2:] == "\r\n":
+        #     print(self.buffer, end="", flush=True)
+        # else:
+        #     print(self.buffer, end="", flush=True)
+
+        print(self.buffer, end="", flush=True)
+
 
     def run(self):
         """Wait for input."""
         new_char = "p"
         while new_char != "q":
             new_char = self._get_char()
+
+            if new_char == "\r":
+                # print('here1')
+                # time.sleep(1)
+                new_char = "\r\n"
+            # if new_char == "\r\n":
+            #     print('here2')
+            #     time.sleep(1)
+
             self.buffer += new_char
             self.paint_screen()
 
