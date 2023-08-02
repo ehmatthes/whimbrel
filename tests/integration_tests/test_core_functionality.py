98951e39e4400a735fb5ce712f64ddee9a2a1a1b
Eric Matthes <ehmatthes@gmail.com>
2023-08-02 09:33:51 -0800

    pytest can be run from root, or any tests subdir.

--- tests/integration_tests/test_core_functionality.py
+++ tests/integration_tests/test_core_functionality.py
@@ -1,14 +1,33 @@
+"""Tests for core functionality of Whimbrel.
+
+This means tests like can it run, and will it not destroy or corrupt files it opens.
+
+It's important to run the first two tests, because they work in combination.
+  For example if the path to whimbrel.py is not correct, the second test will
+  fail for the wrong reason, and due to xfail it will pass. But, the first test 
+  indicates an issue.
+"""
+
+from pathlib import Path
+
 import pexpect
 import pytest
 
 def test_run_whimbrel():
     """Test that I can run Whimbrel, and see expected output."""
-    child = pexpect.spawn('python whimbrel.py')
+    whimbrel_path = Path(__file__).parent.parent.parent / "whimbrel.py"
+    child = pexpect.spawn(f'python {whimbrel_path}')
     child.expect("WHIMBREL", timeout=0.1)
 
 @pytest.mark.xfail
 def test_run_whimbrel_expect_fail():
     """Test that I can run Whimbrel, and fail to see unexpected output."""
-    child = pexpect.spawn('python whimbrel.py')
+    whimbrel_path = Path(__file__).parent.parent.parent / "whimbrel.py"
+    child = pexpect.spawn(f'python {whimbrel_path}')
     child.expect("WHIMBRELLLL", timeout=0.1)
 
+def test_open_file():
+    path = Path(__file__).parent / "reference_files" / "great_birds.txt"
+    print(path)
+    # child = pexpect.spawn('python whimbrel.py ')
+    # child.expect("WHIMBREL", timeout=0.1)
